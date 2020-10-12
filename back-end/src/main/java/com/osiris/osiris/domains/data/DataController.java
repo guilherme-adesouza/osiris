f97f701f3fdef25ee6658b0380bd001f576b0a3a
guilherme-adesouza <guilherme.souza2@universo.univates.br>
2020-10-12 11:06:10 -0300

    fix: wrong endpoint

--- back-end/src/main/java/com/osiris/osiris/domains/data/DataController.java
+++ back-end/src/main/java/com/osiris/osiris/domains/data/DataController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("action")
+@RequestMapping("data")
 public class DataController extends BaseCRUDController<Data> {
 
     @Autowired
